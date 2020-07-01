7727d1fa4be2d8500ac42d9b02638d4e3d4f7868
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-01 13:55:57 +0000

    made index extreeeemly minimal

--- public/script.js
+++ public/script.js
@@ -10,10 +10,26 @@ const paths = {
 
 
 
+const page = content => `
+<html>
+  <head>
+    <meta name="viewport" value="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/style.css">
+    <script src="/__/firebase/7.15.5/firebase-app.js" defer></script>
+    <script src="/__/firebase/init.js" defer></script>
+    <script src="/script.js" defer></script>
+  </head>
+  <body>
+    ${content}
+  </body>
+</html>`
+
+
+
 const load = path => {
   fetch(path)
   .then(response => response.text())
-  .then(text => view.innerHTML = text)
+  .then(text => document.write(page(text)))
 }
 
 
