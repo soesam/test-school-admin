1b9d2835fdb84fcd0e944e290c17aeb32bdb3904
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-01 11:47:16 +0000

    attempted to spa

---
+++ app/paths.js
@@ -0,0 +1,30 @@
+const load = (el, path) => {
+  let xhr = new XMLHttpRequest()
+
+  xhr.onreadystatechange = () => {
+    if (this.readystate === 4 && this.status === 200) {
+      el.innerHTML = xhr.responseText
+    }
+  }
+
+  xhr.open('GET', path, true)
+  xhr.send()
+}
+
+
+
+const paths = {
+  'root': '/views/test.html',
+  'test': '/views/test.html'
+}
+
+
+
+document.body.innerHTML += '<div class="root"></div>'
+const root = document.querySelector('.root')
+
+
+
+window.onhashchange = () => {
+  load(root, window.location.hash.slice('/#/')[1] || 'root')
+}
